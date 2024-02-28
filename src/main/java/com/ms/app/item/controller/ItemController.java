e59426cbe7842cecc0286dc3dd831635bbcd8402
vmauroz <vmauroz@gmail.com>
2024-02-28 18:18:58 -0300

    Clase 6 config server

--- src/main/java/com/ms/app/item/controller/ItemController.java
+++ src/main/java/com/ms/app/item/controller/ItemController.java
@@ -1,12 +1,20 @@
 package com.ms.app.item.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -17,10 +25,20 @@ import com.ms.app.item.model.Item;
 import com.ms.app.item.model.Producto;
 import com.ms.app.item.service.IItemService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
+@RefreshScope
 @RestController
 public class ItemController {
 	
 	private final Logger  logger =  LoggerFactory.getLogger( "ItemController");
+	
+	@Autowired
+	private Environment env;
+	
+	@Value( "${configuracion.texto}")
+	private String texto;
 	
 	@Autowired
 	private CircuitBreakerFactory cbFactory;
@@ -64,6 +82,35 @@ public class ItemController {
 //		return itemServiceFeign.findById(id, cantidad);
 	}	
 	
+	@CircuitBreaker( name = "items", fallbackMethod = "metodoAlternativo")
+	@GetMapping( "/verFeign2/{id}/cantidad/{cantidad}")
+	public Item detalleFeign2( @PathVariable Long id, @PathVariable Integer cantidad) {
+		return itemServiceFeign.findById(id, cantidad); 
+	}
+	
+	@CircuitBreaker( name="items", fallbackMethod = "metodoAlternativo3")
+	@TimeLimiter ( name = "items")
+	@GetMapping( "/verFeign3/{id}/cantidad/{cantidad}")
+	public CompletableFuture<Item> detalleFeign3( @PathVariable Long id, @PathVariable Integer cantidad) {
+		return CompletableFuture.supplyAsync(() -> itemServiceFeign.findById(id, cantidad)); 
+	}
+	
+	@GetMapping( "/obtener-config")
+	public ResponseEntity<?> obtenerConfig( @Value("${server.port}") String puerto) {
+		logger.info(texto);
+		Map<String, String> json = new HashMap<String, String>();
+		
+		json.put( "texto", texto);
+		json.put( "puerto", puerto);
+		
+		if ( env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put( "autor.nombre", env.getProperty("configuracion.autor.name"));
+			json.put( "autor.email",  env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK );
+	}
+	
 	public Item metodoAlternativo( Long id, Integer cantidad, Throwable e) {
 		logger.info( e.getMessage());
 
@@ -77,6 +124,22 @@ public class ItemController {
 		item.setProducto(producto);
 		
 		return item;
+		
+	}
+	
+	public CompletableFuture<Item> metodoAlternativo3( Long id, Integer cantidad, Throwable e) {
+		logger.info( e.getMessage());
+
+		Item item = new Item();
+		Producto producto = new Producto();
+		
+		item.setCantidad( cantidad);
+		producto.setId( id);
+		producto.setName("camara sony manual");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+		
+		return CompletableFuture.supplyAsync(() ->item);
 		
 	}
 }
